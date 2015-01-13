d8b649751d783cce19038cd6ce83d2333bc7f4ad
TERRANZ <terranz@terranz.ath.cx>
2015-01-13 12:30:47 +0400

    editing bean in table

---
+++ shared/src/main/java/ru/terra/dms/constants/URLConstants.java
@@ -0,0 +1,28 @@
+package ru.terra.dms.constants;
+
+/**
+ * Date: 14.07.14
+ * Time: 12:27
+ */
+public class URLConstants {
+    public static final String URL = "http://localhost:8080/dms";
+
+    public static class Objects {
+        public static final String OBJECTS = "/objects";
+        public static final String LIST_BY_NAME = "/do.list.byname.json";
+        public static final String LIST_BY_PARENT = "/do.list.byparent.json";
+    }
+
+    public static class Configuration {
+        public static final String CONFIGURATION = "/configuration";
+    }
+
+
+    public static class Login {
+        public static final String USERS = "/users";
+        public static final String DO_LOGIN = "/do.login.json";
+        public static final String DO_REG = "/do.reg.json";
+        public static final String PARAM_USER = "user";
+        public static final String PARAM_PASS = "pass";
+    }
+}
