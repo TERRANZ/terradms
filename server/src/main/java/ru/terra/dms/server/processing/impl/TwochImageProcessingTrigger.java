d8d9412884d0bb7d76cfc0b33c941de927d87412
Vadim Korostelev <korostelev@smartsolutions-123.ru>
2015-11-13 19:43:36 +0400

    twoch image processing

---
+++ server/src/main/java/ru/terra/dms/server/processing/impl/TwochImageProcessingTrigger.java
@@ -0,0 +1,25 @@
+package ru.terra.dms.server.processing.impl;
+
+import ru.terra.dms.server.processing.Processing;
+import ru.terra.dms.server.processing.ProcessingTrigger;
+
+/**
+ * Created by terranz on 13.11.15.
+ */
+@Processing("TwochImage")
+public class TwochImageProcessingTrigger extends ProcessingTrigger {
+    @Override
+    public void onCreate(Integer objectId) {
+
+    }
+
+    @Override
+    public void onUpdate(Integer objectId) {
+
+    }
+
+    @Override
+    public void onDelete(Integer objectId) {
+
+    }
+}
